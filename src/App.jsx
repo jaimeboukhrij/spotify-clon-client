78f06aeff0d41b87d2a91ec99d4a86e648396a6e
jaime <jaime.boukhrij@smartick.com>
2024-04-22 12:52:43 +0200

    j

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,47 @@ import { useLocation } from 'react-router'
 import './App.css'
 import { Aside } from './components/Aside/Aside'
 import { Nav } from './components/Nav/Nav'
-import { AuthProviderWrapper } from './contexts/auth.context'
+import { AuthContext } from './contexts/auth.context'
 import AppRoutes from './routes/AppRoutes'
 import { GlobalVarProviderWrapper } from './contexts/globalVar.context'
 import { PlayerTrack } from './components/PlayerTrack/PlayerTrack'
 import { TrackPlayProviderWrapper } from './contexts/trackPlaying'
 import { SearchVarProviderWrapper } from './contexts/search.context'
+import { useContext, useEffect } from 'react'
 
 function App () {
+  const { deleteItem } = useContext(AuthContext)
+  useEffect(() => {
+    deleteItem()
+  }, [])
   const location = useLocation()
   const isLoginRoute = location.pathname === '/login' || location.pathname === '/signup'
 
   return (
     <>
-      <AuthProviderWrapper>
-        <TrackPlayProviderWrapper>
-          <GlobalVarProviderWrapper>
-            <SearchVarProviderWrapper>
+      <TrackPlayProviderWrapper>
+        <GlobalVarProviderWrapper>
+          <SearchVarProviderWrapper>
 
-              {!isLoginRoute && <Aside />}
-              <PlayerTrack />
-              <main
-                id='main'
-                style={{
-                  width: '-webkit-fill-available',
-                  height: '--webkit-fill-available',
-                  overflowY: 'hidden',
-                  overflowX: 'hidden',
-                  borderRadius: '7px',
-                  margin: '7px'
-                }}
-              >
-                {!isLoginRoute && <Nav />}
-                <AppRoutes />
-              </main>
-            </SearchVarProviderWrapper>
-          </GlobalVarProviderWrapper>
-        </TrackPlayProviderWrapper>
-      </AuthProviderWrapper>
+            {!isLoginRoute && <Aside />}
+            <PlayerTrack />
+            <main
+              id='main'
+              style={{
+                width: '-webkit-fill-available',
+                height: '--webkit-fill-available',
+                overflowY: 'hidden',
+                overflowX: 'hidden',
+                borderRadius: '7px',
+                margin: '7px'
+              }}
+            >
+              {!isLoginRoute && <Nav />}
+              <AppRoutes />
+            </main>
+          </SearchVarProviderWrapper>
+        </GlobalVarProviderWrapper>
+      </TrackPlayProviderWrapper>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { AuthProviderWrapper } from './contexts/auth.context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Router>
-    <App />
+    <AuthProviderWrapper>
+      <App />
+    </AuthProviderWrapper>
   </Router>
 )
